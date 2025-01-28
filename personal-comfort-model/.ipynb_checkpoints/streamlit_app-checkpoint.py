f91699768e49d7b10a6262aeec966d75c8f5c66c
2071637627 <136345116+2071637627@users.noreply.github.com>
2025-01-28 15:02:46 +0800

    Git LFS commit

---
+++ personal-comfort-model/.ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:06f7557390a910d620efcd07070b107983bd86528497a8c03ab5a92a953d0db0
+size 4173
---
+++ personal-comfort-model/.ipynb_checkpoints/样式-streamlit_app-Copy1-checkpoint.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:377a55c6fdcf05d1f2a140948180fe1897619920af2a4a46d97d5e9bd792b19d
+size 8385
