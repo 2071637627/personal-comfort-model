86574da63100b6fa63d319a2a99ca9fc328d3f56
2071637627 <136345116+2071637627@users.noreply.github.com>
2025-02-10 16:50:51 +0800

    Add files via upload

--- 2025.2.11-personal_comfort_model.py
+++ 2025.2.11-personal_comfort_model.py
@@ -16,7 +16,9 @@ Image.MAX_IMAGE_PIXELS = None
 models = {
     'LightGBM': joblib.load('lgbm_model.pkl'),
     'XGBoost': joblib.load('xgb_model.pkl'),
-    'DT': joblib.load('dt_model.pkl')
+    'DT': joblib.load('dt_model.pkl'),
+    'ET': joblib.load('et_model.pkl'),
+    'RF': joblib.load('rf_model.pkl')
 }
 
 scaler = joblib.load('scaler.pkl')  # 加载训练时保存的归一化器
