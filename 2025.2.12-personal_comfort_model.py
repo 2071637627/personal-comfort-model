3c5972acedd9d7ebd736951a08e5cf22966e2574
2071637627 <136345116+2071637627@users.noreply.github.com>
2025-02-10 19:08:44 +0800

    Add files via upload

--- 2025.2.12-personal_comfort_model.py
+++ 2025.2.12-personal_comfort_model.py
@@ -16,7 +16,6 @@ Image.MAX_IMAGE_PIXELS = None
 models = {
     'LightGBM': joblib.load('lgbm_model.pkl'),
     'XGBoost': joblib.load('xgb_model.pkl'),
-    'DT': joblib.load('dt_model.pkl'),
     'RF': joblib.load('rf_model.pkl')
 }
 
